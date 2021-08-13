05819b76bd8aed82eafacb31595c8e2208d7804a
Giacomo Salici <giambo2000@gmail.com>
2021-08-13 16:50:34 +0200

    db

--- main.py
+++ main.py
@@ -8,8 +8,32 @@ import os
 import psycopg2
 
 
-def start(update, context):
+def start_command(update, context):
     update.message.reply_text('Hi!')
+
+    conn = None
+    try:
+
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(os.environ['DATABASE_URL'])
+
+        cur = conn.cursor()
+
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        db_version = cur.fetchone()
+        print(db_version)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
 
 
 def weather_command(update, context):
@@ -20,57 +44,35 @@ def weather_command(update, context):
 
     response = requests.get(url)
     today_data = json.loads(response.text)["daily"][0]
-    
+
     if float(today_data['weather'][0]['id']) == 800.0:
         icon = icon_set[9]
     else:
         icon = icon_set[round(float(today_data['weather'][0]['id']) / 100)]
 
-    if  datetime.datetime.now().hour<12:
+    if datetime.datetime.now().hour < 12:
         greetings = "Good morning"
-    elif datetime.datetime.now().hour<18:
+    elif datetime.datetime.now().hour < 18:
         greetings = "Good day"
     else:
         greetings = "Good evening"
-    
+
     weather_message = f"{greetings}, today's forecast is for {today_data['weather'][0]['description']} {icon} with temperatures between {round(float(today_data['temp']['min']))} and {round(float(today_data['temp']['max']))}°C."
 
     update.message.reply_text(weather_message)
 
 
 
-def connect():
-    conn = None
-    try:
 
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(os.environ[DATABASE_URL])
-		
-        cur = conn.cursor()
-        
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        db_version = cur.fetchone()
-        print(db_version)
-       
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
 
 
 def main():
-     
     PORT = int(os.environ.get('PORT', 8443))
 
     updater = Updater(os.environ['TG_BOT_TOKEN'], use_context=True)
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", weather_command))
+    dp.add_handler(CommandHandler("start", start_command))
     dp.add_handler(CommandHandler("forecast", weather_command))
 
     updater.start_webhook(listen="0.0.0.0",
